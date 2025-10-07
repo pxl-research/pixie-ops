aab7fc49530aaba4fda794fab0dd02fbb514d24d
Tom Quareme <tom.quareme@pxl.be>
2025-10-07 13:57:27 +0200

    Simplification: no auth token needed anymore

--- experiments/hello_world.py
+++ experiments/hello_world.py
@@ -1,34 +1,4 @@
 from hera.workflows import Workflow, WorkflowsService, DAG, script
-from hera.auth import ArgoCLITokenGenerator
-import urllib3
-import subprocess
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-def get_argo_token(namespace: str = "argo", service_account: str = "hera-submitter") -> str:
-    """
-    Generates a Bearer token for a Kubernetes service account using kubectl.
-    Equivalent to:
-        ARGO_TOKEN="Bearer $(kubectl create token hera-submitter -n argo)"
-
-    Returns:
-        str: The full 'Bearer <token>' string.
-    Raises:
-        RuntimeError: If the token cannot be created.
-    """
-    try:
-        token = subprocess.check_output(
-            ["kubectl", "create", "token", service_account, "-n", namespace],
-            stderr=subprocess.STDOUT,
-        ).strip().decode()
-
-        return f"Bearer {token}"
-
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(
-            f"Failed to generate Argo token: {e.output.decode().strip()}"
-        ) from e
-
 
 @script()
 def echo(message: str):
@@ -42,8 +12,7 @@ with Workflow(
     ttl_seconds_after_finished=3600,
     workflows_service=WorkflowsService(
         host="http://localhost:2746",
-        token=get_argo_token(namespace="argo"),
-        verify_ssl=False,
+        token=None,
         namespace="argo"
     )
 ) as w:
@@ -55,7 +24,7 @@ with Workflow(
         A >> [B, C] >> D
 
 # This submits the workflow to Argo Workflows for execution
-submitted_workflow = w.create()
+submitted_workflow = w.create(wait=True, poll_interval=2)
 
 '''
 # Get a reference to the service
