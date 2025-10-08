d03ff6730baa0ba2a087b34fda5f909a67c99602
Tom Quareme <tom.quareme@pxl.be>
2025-10-08 17:34:16 +0200

    Cleanup

--- README.md
+++ README.md
@@ -14,33 +14,6 @@ curl -sLO "https://github.com/argoproj/argo-workflows/releases/download/v3.7.2/a
 && sudo mv "./argo-linux-amd64" /usr/local/bin/argo \
 && argo version
 
-kubectl create namespace argo
-kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.7.2/install.yaml
-kubectl patch deployment argo-server -n argo --type='json' \
--p='[
-    {"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["server", "--auth-mode=server", "--secure=false"]},
-    {"op": "replace", "path": "/spec/template/spec/containers/0/readinessProbe/httpGet/scheme", "value": "HTTP"}
-]'
-
-TODO: Terraform from here...
-
-kubectl create serviceaccount hera-submitter -n argo
-kubectl create clusterrolebinding argo-default-task-binding \
-    --clusterrole=hera-submitter-role \
-    --serviceaccount=argo:default
-
-kubectl apply -n argo -f hera-binding.yaml
-kubectl apply -n argo -f hera-submitter-role.yaml
-
-
-# Not needed anymore, since done in Python code.
-# export ARGO_TOKEN="Bearer $(kubectl create token hera-submitter -n argo)"
-
-kubectl -n argo port-forward service/argo-server 2746:2746
-kubectl -n argo port-forward svc/argo-workflows-server 2746:2746 via Terraform
-
-python3 hello_world.py
-kubectl get wf -n argo
 ```
 
 * OpenTofu:
@@ -54,5 +27,10 @@ minikube start --cpus 2 --memory 2048mb --driver=docker
 tofu init
 tofu plan
 tofu apply -auto-approve
+kubectl -n argo port-forward svc/argo-workflows-server 2746:2746
+python3 hello_world.py
+kubectl get wf -n argo
+
+TODO: via FastAPI:
 curl -X POST $(minikube ip):30080/trigger
 ````
--- infrastructure/local/main.tf
+++ infrastructure/local/main.tf
@@ -10,6 +10,9 @@ resource "kubernetes_namespace" "argo_namespace" {
 }
 
 resource "helm_release" "argo_workflows" {
+  depends_on = [
+    kubernetes_namespace.argo_namespace
+  ]
   provider   = helm.minikube_conn 
   name       = "argo-workflows"
   repository = "https://argoproj.github.io/argo-helm"
@@ -21,6 +24,9 @@ resource "helm_release" "argo_workflows" {
 }
 
 resource "kubectl_manifest" "hera_rbac" {
+  depends_on = [
+    helm_release.argo_workflows
+  ]
   for_each = {
     serviceaccount = "${path.module}/../../kubernetes/base/hera-submitter-sa.yaml"
     clusterrole    = "${path.module}/../../kubernetes/base/hera-submitter-role.yaml"
@@ -35,122 +41,3 @@ resource "kubectl_manifest" "hera_rbac" {
     kubernetes_namespace.argo_namespace
   ]
 }
-
-
-/* resource "null_resource" "install_argo" {
-  depends_on = [
-    kubernetes_namespace.argo_namespace
-  ]
-
-  provisioner "local-exec" {
-    command = "kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.7.2/install.yaml"
-  }
-}
- */
-
-
-# # 6. Hera RBAC and service account
-# resource "null_resource" "hera_rbac" {
-#   depends_on = [null_resource.install_argo]
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-# # Create Hera service account
-# kubectl create serviceaccount hera-submitter -n argo || echo "ServiceAccount exists"
-
-# # Bind cluster role
-# kubectl create clusterrolebinding argo-default-task-binding \
-#   --clusterrole=hera-submitter-role \
-#   --serviceaccount=argo:default || echo "Binding exists"
-
-# # Apply Hera manifests
-# kubectl apply -n argo -f ${path.module}/../../kubernetes/base/hera-binding.yaml
-# kubectl apply -n argo -f ${path.module}/../../kubernetes/base/hera-submitter-role.yaml
-
-# # Patch argo-server deployment for server auth
-# kubectl patch deployment argo-server -n argo --type='json' \
-#   -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--auth-mode=server"}]'
-# EOT
-#   }
-# }
-
-# # Capture the Argo token as a Terraform variable
-# data "external" "argo_token" {
-#   depends_on = [null_resource.hera_rbac]
-
-#   program = [
-#     "bash",
-#     "-c",
-#     <<EOF
-# set -e
-# token=$(kubectl create token hera-submitter -n argo)
-# echo "{\"argo_token\": \"Bearer $${token}\"}"
-# EOF
-#   ]
-# }
-
-
-# # 0. Execute minikube setup script
-# resource "null_resource" "setup_minikube" {
-#   depends_on = [
-#     null_resource.hera_rbac,
-#     data.external.argo_token
-#   ]
-  
-#   triggers = {
-#     always_run = timestamp()
-#   }
-
-#   provisioner "local-exec" {
-#     environment = {
-#       ARGO_TOKEN = data.external.argo_token.result.argo_token
-#     }
-#     command = "bash ./minikube_setup.sh"
-#     when    = create
-#   }
-# }
-
-# # 1. Create the Kubernetes Namespace for the application
-# resource "kubernetes_manifest" "pixie_namespace" {
-#   depends_on = [null_resource.setup_minikube, null_resource.install_argo, null_resource.hera_rbac]
-
-#   manifest = {
-#     apiVersion = "v1"
-#     kind       = "Namespace"
-#     metadata = {
-#       name = "pixie"
-#     }
-#   }
-# }
-
-# # 2. Apply Deployment Manifest
-# resource "kubernetes_manifest" "app_deployment" {
-#   depends_on = [
-#     kubernetes_manifest.pixie_namespace,
-#     null_resource.install_argo,
-#     null_resource.hera_rbac
-#   ]
-
-#   manifest = yamldecode(
-#     templatefile("${path.module}/../../kubernetes/base/deployment.yaml", {
-#       app_name   = local.app_name
-#       image_name = var.image_name
-#       image_tag  = var.image_tag
-#     })
-#   )
-# }
-
-# # 3. Apply Service Manifest
-# resource "kubernetes_manifest" "app_service" {
-#   depends_on = [
-#     kubernetes_manifest.app_deployment,
-#     null_resource.install_argo,
-#     null_resource.hera_rbac
-#   ]
-
-#   manifest = yamldecode(
-#     templatefile("${path.module}/../../kubernetes/base/service.yaml", {
-#       app_name = local.app_name
-#     })
-#   )
-# }
