6ff6e52e6585fb37f7f8bb4c213937c2504fa64d
Tom Quareme <tom.quareme@pxl.be>
2025-10-29 15:01:06 +0100

    Minimal layer on top of Terraform & Kubernetes

--- infrastructure/environments/development/locals.tf
+++ infrastructure/environments/development/locals.tf
@@ -3,32 +3,96 @@ locals {
   cluster_name = "pixie"
   argo_namespace_name = "argo"
   pixie_namespace_name = "pixie"
-  ingest_server_app_name = "pixie-ingest"
-  ingest_server_image_name = "pixie-ingest"
-  ingest_server_image_tag = "1.0.0"
-  
+  is_local_deployment = true
+
   # General paths
   apps_path = "${path.module}/../../../apps"
   k8s_base_path = "${path.module}/../../../kubernetes"
-  
+
   # Ingress
   ingress_version = "4.7.1"
   ingress_host = "localhost"
   ingress_namespace_name = "ingress-nginx"
-  
+
   # Packages
   argo_workflows_version = "0.45.26" # this is 3.7.2 outside of helm
 
   # Apps specific paths
-  # TODO: make a more generic hashmap out of this
+  # TODO: make a more generic hashmap out of this, dividing it in deployment/statefulset, service, ingress submaps
   # Ingest server
-  ingest_server_app_path = "${local.apps_path}/ingest_server"
-  ingest_server_ingress_path = "/ingest" # root path for this service
+  # deployment, service, ingress
+  ingest_server_app_name = "pixie-ingest"
   ingest_server_target_port = 8000
+  # Docker (put this under deployment?)
+  ingest_server_app_path = "${local.apps_path}/ingest_server" # Docker image
+  # deployment.yaml or statefulset.yaml
   ingest_server_replica_count = 1
   ingest_server_has_probing = true
+  ingest_server_image_name = "pixie-ingest"
+  ingest_server_image_tag = "1.0.0"
   ingest_server_request_cpu = "128m"
   ingest_server_request_memory = "256Mi"
   ingest_server_limit_cpu = "256m"
   ingest_server_limit_memory = "1Gi"
+  #ingest_server_request_storage = "10Gi" # only for statefulset.yaml
+  # ingress.yaml
+  ingest_server_ingress_path = "/ingest" # root path for this service
+
+  # Application Configuration Map (The structure for dynamic deployment)
+  app_configs = {
+    # Key 'ingest_server' is used as the resource instance identifier (e.g., docker_image.app["ingest_server"])
+    ingest_server = {
+      metadata = {
+        app_name    = "pixie-ingest"
+        target_port = 8000
+      }
+      deployment = {
+        replica_count     = 1
+        has_probing       = true
+        image_name        = "pixie-ingest"
+        image_tag         = "1.0.1"
+        docker_build_path = "${local.apps_path}/ingest_server"
+        request_cpu       = "128m"
+        request_memory    = "256Mi"
+        limit_cpu         = "256m"
+        limit_memory      = "1Gi"
+      }
+      service = {
+        type = "ClusterIP"
+      }
+      ingress = {
+        enabled = true
+        path    = "/ingest"
+      }
+    }
+  }
+
+  /*
+  {
+    "metadata": {
+      "app_name": "pixie-ingest",
+      "namespace_name": "pixie",
+      "target_port": 8000, # container port
+    }
+    "deployment": { # or "statefulset"
+      "replica_count": 1,
+      "has_probing": true,
+      "image_name": "pixie-ingest",
+      "image_tag": "1.0.0",
+      "docker_build_path": "${local.apps_path}/ingest_server",
+      "request_cpu": "128m",
+      "request_memory": "256Mi",
+      "limit_cpu": "256m",
+      "limit_memory": "1Gi",
+      # "request_storage": "10Gi" # only for statefulset
+    },
+    "service": {
+      "type": "ClusterIP"
+    },
+    "ingress": {
+      "enabled": true,
+      "path": "/ingest"
+    }
+}
+  */
 }
--- infrastructure/environments/development/main.tf
+++ infrastructure/environments/development/main.tf
@@ -1,16 +1,230 @@
+
+resource "kubectl_manifest" "pixie_namespace" {
+  yaml_body = templatefile("${local.k8s_base_path}/namespace.yaml", {
+    namespace_name        = local.pixie_namespace_name
+  })
+
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+
+
+########################################
+# HELM RELEASES
+########################################
+resource "helm_release" "argo_workflows" {
+  name             = "argo-workflows"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-workflows"
+  version          = local.argo_workflows_version
+  namespace        = local.argo_namespace_name
+  create_namespace = true
+
+  values = [file("${local.k8s_base_path}/argo-workflows-values.yaml")]
+
+  depends_on = [
+    kubectl_manifest.pixie_namespace
+  ]
+}
+
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = local.ingress_version
+  namespace        = local.ingress_namespace_name
+  create_namespace = true
+
+  values = [file("${local.k8s_base_path}/ingress-nginx-values.yaml")]
+
+  depends_on = [
+    kubectl_manifest.pixie_namespace
+  ]
+}
+
+########################################
+# WAIT FOR INGRESS CONTROLLER
+########################################
+resource "null_resource" "wait_for_ingress_nginx" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Waiting for the nginx ingress controller...' && kubectl wait --namespace ${helm_release.ingress_nginx.namespace} --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=600s"
+  }
+
+  depends_on = [helm_release.ingress_nginx]
+}
+########################################
+# RBAC CONFIGURATION
+########################################
+resource "kubectl_manifest" "hera_rbac" {
+  for_each = {
+    serviceaccount  = "${local.k8s_base_path}/hera-submitter-sa.yaml"
+    clusterrole     = "${local.k8s_base_path}/hera-submitter-role.yaml"
+    binding_hera    = "${local.k8s_base_path}/hera-submitter-binding.yaml"
+    binding_default = "${local.k8s_base_path}/argo-default-task-binding.yaml"
+  }
+
+  yaml_body = file(each.value)
+
+  depends_on = [
+    helm_release.argo_workflows,
+    null_resource.wait_for_ingress_nginx
+  ]
+}
+
+
+########################################
+# DYNAMIC APP DEPLOYMENT (IMAGE BUILD, LOAD, K8S MANIFESTS)
+########################################
+
+# 1. Build the local Docker image for each app using for_each
+resource "docker_image" "app" {
+  for_each = local.app_configs
+
+  # Access configuration using each.value
+  name = "${each.value.deployment.image_name}:${each.value.deployment.image_tag}"
+
+  build {
+    context    = abspath(local.apps_path)
+    dockerfile = "${each.value.deployment.docker_build_path}/Dockerfile"
+  }
+
+  depends_on = [kubectl_manifest.hera_rbac]
+}
+
+# 2. Load the image into Kind for each app using for_each
+resource "null_resource" "kind_image_load_app" {
+  for_each = local.app_configs
+
+  triggers = {
+    # Reference the specific image instance using the key: docker_image.app[each.key]
+    image_id     = docker_image.app[each.key].image_id
+    cluster_name = kind_cluster.default.name
+  }
+
+  provisioner "local-exec" {
+    command = "kind load docker-image ${docker_image.app[each.key].name} --name ${kind_cluster.default.name}"
+  }
+
+  depends_on = [
+    # FIX: Remove [each.key] to satisfy static reference requirement.
+    docker_image.app,
+    kind_cluster.default
+  ]
+}
+
+# 3. Rollout trigger (to redeploy on image rebuild) for each app using for_each
+resource "null_resource" "rollout_trigger" {
+  for_each = local.app_configs
+
+  triggers = {
+    timestamp = timestamp()
+  }
+
+  # FIX: Remove [each.key] to satisfy static reference requirement.
+  depends_on = [null_resource.kind_image_load_app]
+}
+
+# 4. Create Kubernetes Deployment for each app using for_each
+resource "kubectl_manifest" "app_deployment" {
+  for_each = local.app_configs
+
+  yaml_body = templatefile("${local.k8s_base_path}/deployment.yaml", {
+    app_name            = each.value.metadata.app_name
+    namespace_name      = local.pixie_namespace_name
+    is_local_deployment = local.is_local_deployment
+    target_port         = each.value.metadata.target_port
+
+    image_name          = each.value.deployment.image_name
+    image_tag           = each.value.deployment.image_tag
+    # Accessing the correct trigger instance by its key: null_resource.rollout_trigger[each.key]
+    rollout_trigger     = null_resource.rollout_trigger[each.key].triggers.timestamp
+    image_pull_secret_name = ""
+
+    replica_count       = each.value.deployment.replica_count
+    has_probing         = each.value.deployment.has_probing
+    request_cpu         = each.value.deployment.request_cpu
+    request_memory      = each.value.deployment.request_memory
+    limit_cpu           = each.value.deployment.limit_cpu
+    limit_memory        = each.value.deployment.limit_memory
+  })
+
+  wait = false
+
+  depends_on = [
+    # FIX: Remove [each.key] to satisfy static reference requirement.
+    null_resource.kind_image_load_app,
+    null_resource.rollout_trigger
+  ]
+}
+
+# 5. Create Kubernetes Service for each app using for_each
+resource "kubectl_manifest" "app_service" {
+  for_each = local.app_configs
+
+  yaml_body = templatefile("${local.k8s_base_path}/service.yaml", {
+    app_name          = each.value.metadata.app_name
+    namespace_name    = local.pixie_namespace_name
+    is_local_deployment = local.is_local_deployment
+    target_port       = each.value.metadata.target_port
+  })
+
+  # FIX: Remove [each.key] to satisfy static reference requirement.
+  depends_on = [kubectl_manifest.app_deployment]
+}
+
+# 6. Create Kubernetes Ingress for each app that has ingress enabled
+resource "kubectl_manifest" "app_ingress" {
+  # Filter the map to only include apps where ingress is enabled
+  for_each = {
+    for k, v in local.app_configs : k => v
+    if v.ingress.enabled
+  }
+
+  yaml_body = templatefile("${local.k8s_base_path}/ingress.yaml", {
+    app_name          = each.value.metadata.app_name
+    namespace_name    = local.pixie_namespace_name
+    ingress_host      = local.ingress_host
+    ingress_path      = each.value.ingress.path
+  })
+
+  # FIX: Remove [each.key] to satisfy static reference requirement.
+  depends_on = [
+    kubectl_manifest.app_service,
+    helm_release.ingress_nginx
+  ]
+}
+
+########################################
+# HERA BASE IMAGE LOAD
+########################################
+
+resource "null_resource" "kind_image_load_hera_echo_base_image" {
+  triggers = {
+    cluster_name = kind_cluster.default.name
+  }
+
+  provisioner "local-exec" {
+    command = "kind load docker-image python:3.11-alpine --name ${kind_cluster.default.name}"
+  }
+
+  depends_on = [
+    # Depend on the ingress of the specific 'ingest_server' app using the map key
+    kubectl_manifest.app_ingress["ingest_server"],
+    kind_cluster.default
+  ]
+}
+
+
+
+/*
 ########################################
 # NAMESPACE CREATION
 ########################################
-#resource "kubernetes_namespace" "pixie_namespace" {
-#  metadata {
-#    name = local.pixie_namespace_name
-#  }
-#
-#  depends_on = [
-#    kind_cluster.default
-#  ]
-#}
-
 resource "kubectl_manifest" "pixie_namespace" {
   yaml_body = templatefile("${local.k8s_base_path}/namespace.yaml", {
     namespace_name          = local.pixie_namespace_name
@@ -64,7 +278,7 @@ resource "null_resource" "wait_for_ingress_nginx" {
   }
 
   provisioner "local-exec" {
-    command = "echo 'Waiting for the nginx ingress controller...' && kubectl wait --namespace ${helm_release.ingress_nginx.namespace} --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s"
+    command = "echo 'Waiting for the nginx ingress controller...' && kubectl wait --namespace ${helm_release.ingress_nginx.namespace} --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=6s"
   }
 
   depends_on = [helm_release.ingress_nginx]
@@ -140,12 +354,14 @@ resource "kubectl_manifest" "ingest_server_deployment" {
   yaml_body = templatefile("${local.k8s_base_path}/deployment.yaml", {
     app_name                = local.ingest_server_app_name
     namespace_name          = local.pixie_namespace_name
+    is_local_deployment     = true
+    target_port             = local.ingest_server_target_port
+
     image_name              = local.ingest_server_image_name
     image_tag               = local.ingest_server_image_tag
     rollout_trigger         = null_resource.rollout_trigger.triggers.timestamp
-    is_local_deployment     = true
     image_pull_secret_name  = ""
-    target_port             = local.ingest_server_target_port
+
     replica_count           = local.ingest_server_replica_count
     has_probing             = local.ingest_server_has_probing
     request_cpu             = local.ingest_server_request_cpu
@@ -168,7 +384,7 @@ resource "kubectl_manifest" "ingest_server_service" {
     app_name       = local.ingest_server_app_name
     namespace_name = local.pixie_namespace_name
     is_local_deployment = true
-    app_target_port = local.ingest_server_target_port
+    target_port = local.ingest_server_target_port
   })
 
   depends_on = [kubectl_manifest.ingest_server_deployment]
@@ -180,7 +396,7 @@ resource "kubectl_manifest" "ingest_server_ingress" {
     app_name       = local.ingest_server_app_name
     namespace_name = local.pixie_namespace_name
     ingress_host   = local.ingress_host
-    ingress_path   = local.ingest_server_ingress_path 
+    ingress_path   = local.ingest_server_ingress_path
   })
 
   depends_on = [
@@ -207,3 +423,4 @@ resource "null_resource" "kind_image_load_hera_echo_base_image" {
     kind_cluster.default
   ]
 }
+*/
